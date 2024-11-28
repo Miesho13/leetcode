e7e740123866a03b405f2fc225ca0b5cb7d80bde
Marcin <marcin.ryzewskii@gmail.com>
2024-11-28 11:21:24 +0100

    Add solution for min_stack

--- stack/mini_stack/solution.cc
+++ stack/mini_stack/solution.cc
@@ -11,33 +11,54 @@
 
 class MinStack {
 public:
-    uint8_t stack_buffer[1024];
-    uint32_t sp = 0;
-
     MinStack() {
-        
+          
     }
-    
+
     void push(int val) {
-        
+        main_stack.push(val);
+
+        if (min_stack.empty()) {
+            min_stack.push(val);
+        }
+        else {
+            if (min_stack.top() >= val) {
+                min_stack.push(val);
+            }
+        }
     }
     
     void pop() {
-        
+        if (main_stack.top() == min_stack.top()) {
+            min_stack.pop();
+        }
+        main_stack.pop();
     }
     
     int top() {
-        
+        return main_stack.top();
     }
-    
-    int getMin() {
-        
+
+    int get_min() {
+        return min_stack.top();
     }
+
+
+    private:
+    std::stack<int> main_stack;
+    std::stack<int> min_stack;
 };
 
-
 int main() {
-    MinStack stack;
-    
+    MinStack minStack; 
+    minStack.push(1);
+    minStack.push(2);
+    minStack.push(0);
+
+    std::cout << minStack.get_min() << '\n'; // return 0
+    minStack.pop();
+    std::cout << minStack.top() << '\n';    // return 2
+    std::cout << minStack.get_min() << '\n'; // return 1
+
     return 0;
 }
